84885a5d8a634d3f64f17b778dab31c8789b8647
arcolinuxz <arcolinuxinfo@gmail.com>
2024-04-20 07:59:47 +0200

    update

--- repo.sh
+++ repo.sh
@@ -25,9 +25,13 @@ sleep 2
 echo "repo-add"
 repo-add -n -R -v -s nemesis_repo.db.tar.gz *.pkg.tar.zst
 rm -v nemesis_repo.db
+rm -v nemesis_repo.db.sig
 rm -v nemesis_repo.files
+rm -v nemesis_repo.files.sig
 mv -v nemesis_repo.db.tar.gz nemesis_repo.db
+mv -v nemesis_repo.db.tar.gz.sig nemesis_repo.db.sig
 mv -v nemesis_repo.files.tar.gz nemesis_repo.files
+mv -v nemesis_repo.files.tar.gz.sig nemesis_repo.files.sig
 cd ..
 echo "####################################"
 echo "Repo Updated!!"
