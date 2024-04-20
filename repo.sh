8e88594f98706d20c3b1c5360dd2a9108404cd19
arcolinuxz <arcolinuxinfo@gmail.com>
2024-04-20 07:35:06 +0200

    udpate

--- repo.sh
+++ repo.sh
@@ -5,10 +5,10 @@ rm nemesis_repo*
 
 echo "repo-add"
 repo-add -n -R nemesis_repo.db.tar.gz *.pkg.tar.zst
-rm nemesis_repo.db
-rm nemesis_repo.files
-mv nemesis_repo.db.tar.gz nemesis_repo.db
-mv nemesis_repo.files.tar.gz nemesis_repo.files
+rm -v nemesis_repo.db
+rm -v nemesis_repo.files
+mv -v nemesis_repo.db.tar.gz nemesis_repo.db
+mv -v nemesis_repo.files.tar.gz nemesis_repo.files
 cd ..
 echo "####################################"
 echo "Repo Updated!!"
