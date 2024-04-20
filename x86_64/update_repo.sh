64bfed0a904792a8c4f4dfcae3b222c301e49bed
arcolinuxz <arcolinuxinfo@gmail.com>
2024-04-20 05:40:18 +0200

    update

--- x86_64/update_repo.sh
+++ x86_64/update_repo.sh
@@ -7,16 +7,16 @@ repo-add -n -R nemesis_repo.db.tar.gz *.pkg.tar.zst
 sleep 5
 
 rm nemesis_repo.db
-rm nemesis_repo.db.sig
+
 
 rm nemesis_repo.files
-rm nemesis_repo.files.sig
+
 
 mv nemesis_repo.db.tar.gz nemesis_repo.db
-mv nemesis_repo.db.tar.gz.sig nemesis_repo.db.sig
+
 
 mv nemesis_repo.files.tar.gz nemesis_repo.files
-mv nemesis_repo.files.tar.gz.sig nemesis_repo.files.sig
+
 
 echo "####################################"
 echo "Repo Updated!!"
