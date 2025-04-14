86044f05c0d1fd50f3a76852a68704e5f4f62d83
Erik Dubois <erik.dubois@gmail.com>
2025-04-14 09:56:09 +0200

    update

--- up.sh
+++ up.sh
@@ -37,23 +37,6 @@
 # https://www.baeldung.com/ops/git-remove-file-without-deleting-it
 # git rm --cached file.txt
 
-# Creating the databases
-sh repo.sh
-
-# Below command will backup everything inside the project folder
-git add --all .
-
-# Give a comment to the commit if you want
-echo "####################################"
-echo "Write your commit comment!"
-echo "####################################"
-
-read input
-
-# Committing to the local repository with a message containing the time details and commit text
-
-git commit -m "$input"
-
 MAX_SIZE_MB=95
 FOLDER="x86_64"
 
@@ -70,6 +53,23 @@ for file in "$FOLDER"/*.pkg.tar.zst; do
 done
 
 echo "All package files are within the 95MB limit."
+
+# Creating the databases
+sh repo.sh
+
+# Below command will backup everything inside the project folder
+git add --all .
+
+# Give a comment to the commit if you want
+echo "####################################"
+echo "Write your commit comment!"
+echo "####################################"
+
+read input
+
+# Committing to the local repository with a message containing the time details and commit text
+
+git commit -m "$input"
 
 # Push the local files to github
 
