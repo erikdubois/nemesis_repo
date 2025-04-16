44ee21c6e163e8d163d7b37e250ddaef30dba8f4
Erik Dubois <erik.dubois@gmail.com>
2025-04-16 10:14:02 +0200

    update

--- up.sh
+++ up.sh
@@ -39,11 +39,10 @@ echo "####################################"
 echo "Write your commit comment!"
 echo "####################################"
 
-read input
 
 # Committing to the local repository with a message containing the time details and commit text
 
-git commit -m "$input"
+git commit -m "update"
 
 # Push the local files to github
 
