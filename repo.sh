69272835dfaa881d93f283dfcc7c5e06ddfbcaf1
Erik Dubois <erik.dubois@gmail.com>
2025-04-16 18:46:26 +0200

    update

--- repo.sh
+++ repo.sh
@@ -4,7 +4,7 @@ cd x86_64
 rm nemesis_repo*
 
 echo "repo-add"
-repo-add -n -R -v -s nemesis_repo.db.tar.gz *.pkg.tar.zst
+repo-add -n -R -v nemesis_repo.db.tar.gz *.pkg.tar.zst
 rm -v nemesis_repo.db
 rm -v nemesis_repo.db.sig
 rm -v nemesis_repo.files
